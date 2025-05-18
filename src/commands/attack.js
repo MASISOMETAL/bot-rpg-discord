70654d46ebb563d63236215ad03584f309e54bd0
MASISOMETAL <cristianmasisofarias@gmail.com>
2025-05-18 12:02:56 -0300

    segundo intento de arreglo

--- src/commands/attack.js
+++ src/commands/attack.js
@@ -33,7 +33,7 @@ export default {
       return;
     }
 
-    await interaction.deferReply();
+    await interaction.deferReply({ fetchReply: true });
     const userId = interaction.user.id;
     const serverId = interaction.guild.id;
     const monster_id = interaction.options.getInteger('monster_id');
